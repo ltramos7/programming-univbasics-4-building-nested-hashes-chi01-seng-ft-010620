00be209375aa679eccf376f288314a27f40ddd32
Linda Ramos <ltramos7@gmail.com>
2019-12-18 16:37:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
   # complexity.
   hash = {:railroads => {:rent_in_dollars => {
                                               :one_piece_owned => 25, 
-                                              :key2 => "value2", 
+                                              :two_pieces_owned => 50, 
                                               :key3 => "value3", 
                                               :key4 => "value4"}, 
                          :names => {
